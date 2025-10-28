0f8686d2dfb7e39447df23e5fecbbc191f71de75
TheRVP <rubeshvp@baggmobil.onmicrosoft.com>
2025-10-28 16:42:29 +0530

    Create a backup manifest file
    The manifest file will have a summary of the files that were processed during the backup process.
    
    This requires Newtonsoft.Json package to be installed.

---
+++ An2WinFileTransfer/An2WinFileTransfer/Models/BackupFileEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using An2WinFileTransfer.Enums;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace An2WinFileTransfer.Models
+{
+    public class BackupFileEntry
+    {
+        public string RelativePath { get; set; } = string.Empty;
+        public ulong Size { get; set; }
+        public DateTime LastWriteTime { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public ECopyStatus CopyStatus { get; set; }
+    }
+}
---
+++ An2WinFileTransfer/An2WinFileTransfer/Models/BackupManifest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace An2WinFileTransfer.Models
+{
+    public class BackupManifest
+    {
+        public DateTime BackupTime { get; set; }
+        public string SourceRoot { get; set; } = string.Empty;
+        public List<BackupFileEntry> Files { get; set; } = new List<BackupFileEntry>();
+        public TimeSpan BackupDuration { get; set; }
+    }
+}
